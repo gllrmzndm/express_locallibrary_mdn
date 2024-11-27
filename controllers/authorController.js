5b507032557741214d789816aa00ae4544a48328
gllrmzndm <guillermozaandam@hotmail.com>
2024-11-27 11:37:04 +0100

    https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/forms/Create_author_form

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,11 @@
 const Author = require("../models/author");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+// Display Author create form on GET.
+exports.author_create_get = (req, res, next) => {
+  res.render("author_form", { title: "Create Author" });
+};
 
 // Display list of all authors
 exports.author_list = asyncHandler(async (req, res, next) => {
@@ -21,9 +27,56 @@ exports.author_create_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle Author create on POST.
-exports.author_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
-});
+exports.author_create_post = [
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape.withMessage("First name must be specified")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  body("date_of_death", "invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitazation.
+  asyncHandler(async (req, res, next) => {
+    // Extract  the validation errors from a rquest.
+    const errors = validationResult(req);
+
+    // Create  Author object with escaped and  trimmed data
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+
+    if (!error.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("author_form", {
+        title: "Create Author",
+        author: "author",
+        errors: "errors.array(),",
+      });
+      return;
+    } else {
+      // Data from form is valid
+
+      // Save author.
+      await author.save();
+      // Redirect to new author record.
+      res.redirect(author.url);
+    }
+  }),
+];
 
 // Display Author delete form on GET.
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
